2c118db59c89955289e5e36c72bbe70296ee15ec
lamamalaga <tomchuk.01@gmail.com>
2019-10-27 21:12:25 +0500

    second commit

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите свою электронную почту:");  
+            var str = Console.ReadLine();
+            var index = str.IndexOf('@');
+            var mailBox = str.Substring(0, index);
+            var mailservice = str.Substring(index + 1);
+
+
+            Console.WriteLine("Ваш почтовый ящик {0} находиться на сервисе {1}", mailBox, mailservice); 
+
+        }
+
+        }
+    }
+
+    
